39e1d1c43d0f288b2811d0c9831440678fa31a49
ehallvard <haer@steria.no>
2014-03-25 16:53:35 +0100

    Laget meny for mobilstørrelse

---
+++ helsedir/js/sohoFont.js
@@ -0,0 +1,10 @@
+﻿            (function() {
+                var path = '//easy.myfonts.net/v2/js?sid=212833(font-family=Soho+Gothic+Pro+Light)&sid=212835(font-family=Soho+Gothic+Pro+Regular)&sid=212837(font-family=Soho+Gothic+Pro+Medium)&sid=212839(font-family=Soho+Gothic+Pro+Bold)&key=luwqtZRINA',
+                    protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+                    trial = document.createElement('script');
+                trial.type = 'text/javascript';
+                trial.async = true;
+                trial.src = protocol + path;
+                var head = document.getElementsByTagName("head")[0];
+                head.appendChild(trial);
+            })();
