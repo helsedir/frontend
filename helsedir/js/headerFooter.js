49aee6117e0e42eea6f694c39b00f7e1c152fef4
ehallvard <haer@steria.no>
2014-04-14 10:10:29 +0200

    Gjort forandringer til hovedmeny, samt litt UU

--- helsedir/js/headerFooter.js
+++ helsedir/js/headerFooter.js
@@ -1,8 +1,8 @@
-﻿$(document).ready(function() {
-    $("#header").load("header.html", function() {
-        $("#tabs").tabs();
-        $("#statistikk").tabs();
+﻿$(document).ready(function () {
+    $("#header").load("header.html", function () {
         $.getScript('../js/expand.js', function () {
+        });
+        $.getScript('../js/test.js', function () {
         });
     });
     $("#footer").load("footer.html");
