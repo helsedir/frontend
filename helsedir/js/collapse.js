8d8a1ec4162ef9a1978e66ffcf95b442013a3bf4
ehallvard <haer@steria.no>
2014-04-23 12:59:27 +0200

    Fikset IE8-støtte for javascript

---
+++ helsedir/js/collapse.js
@@ -0,0 +1,18 @@
+﻿$(".collapse").click(function () {
+    $header = $(this);
+    $content = $header.next();
+    $parent = $header.parent();
+
+    $content.toggleClass('visuallyhidden');
+    if ($parent.hasClass("has-hidden-content")) {
+        $parent.addClass("has-visible-content");
+        $parent.removeClass("has-hidden-content");
+    } else {
+        $parent.removeClass("has-visible-content");
+        $parent.addClass("has-hidden-content");
+    }
+});
+
+$(function () {
+    $(".collapse").parent().addClass("has-hidden-content");
+});
--- helsedir/js/toggle.js
+++ helsedir/js/toggle.js
@@ -1,41 +1,31 @@
 $(document).ready(function () {
-
     "use strict";
 
-    var mq = window.matchMedia("(max-width: 600px)");
+    var mq = Modernizr.mq("(max-width: 600px)");
 
     var togglers = $(".js-toggler"),
         toggledContent = togglers.nextAll(),
         toggledContentParent = togglers.parent();
 
-
     function hideContentOnNarrow() {
-
         var narrow = mq.matches;
-
-        console.log(narrow);
-
 
         if (narrow) {
             toggledContent.addClass("visuallyhidden");
             togglers.addClass("pseudo-link");
             toggledContentParent.addClass("has-hidden-content");
-
         } else {
             toggledContent.removeClass("visuallyhidden");
             togglers.removeClass("pseudo-link");
             toggledContentParent.removeClass("has-visible-content").removeClass("has-hidden-content");
         }
-
     }
 
     function enableToggleForNarrow() {
         togglers.on('click', function () {
-
             var narrow = mq.matches;
 
             if (narrow) {
-
                 var clickedElement = $(this),
                     clickedElementParent = clickedElement.parent();
 
@@ -57,5 +47,4 @@ $(document).ready(function () {
 
     $(window).resize(hideContentOnNarrow);
     $(window).on("orientationchange", hideContentOnNarrow);
-
 });
