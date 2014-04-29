d9fda0724e38e05e1a9be02ecb6ead1aa2b45e3a
Tor Anders Dybing <tadybing@gmail.com>
2014-04-29 10:55:45 +0200

    Endring på header/footer

--- helsedir/js/headerFooter.js
+++ helsedir/js/headerFooter.js
@@ -1,9 +1,9 @@
 ﻿$(document).ready(function () {
-    $("#header").load("header.html", function () {
+    $("#header").load("http://helsedir.azurewebsites.net/layouts/header.html", function () {
         $.getScript('../js/expand.js', function () {
         });
         $.getScript('../js/tabs.js', function () {
         });
     });
-    $("#footer").load("footer.html");
+    $("#footer").load("http://helsedir.azurewebsites.net/layouts/footer.html");
 });
