112f229d0cb6cf1ad07e0de39a5c7200d5f60284
Tor Anders Dybing <tadybing@gmail.com>
2014-04-29 13:36:01 +0200

    Headeroppdatering

---
+++ helsedir/js/headerFooterRetningslinje.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("#header").load("http://helsedir.azurewebsites.net/layouts/header.html", function () {
+        $.getScript('http://helsedir.azurewebsites.net/js/expand.js', function () {
+        });
+        $.getScript('http://helsedir.azurewebsites.net/js/tabs.js', function () {
+        });
+    });
+    $("#footer").load("http://helsedir.azurewebsites.net/layouts/footer.html");
+});
