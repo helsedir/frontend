0b1b99bdbc84a397aff9d580433060c1e9d9af17
Tor Anders Dybing <tadybing@gmail.com>
2014-05-05 22:33:22 +0200

    Oppdatering av retningslinjer

--- helsedir/js/retningslinje.js
+++ helsedir/js/retningslinje.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function() {
   var width = jQuery(window).width();
 
   //if (width < 625) {
-  	jQuery('.recommendation_intro').siblings('.recommendation_extras').addClass('hidden');
+  	jQuery('.recommendation_intro').siblings('.recommendation_text').addClass('hidden');
   	jQuery('.hide-on-narrow').nextAll().addClass('hidden');
   //}
   
@@ -15,8 +15,8 @@ jQuery(document).ready(function() {
 
     //if (width < 625) {
       jQuery('.recommendation_intro').addClass('accordion');
-      jQuery('.recommendation_intro.open').siblings('.recommendation_extras').removeClass('hidden');
-      jQuery('.recommendation_intro.closed').siblings('.recommendation_extras').addClass('hidden');
+      jQuery('.recommendation_intro.open').siblings('.recommendation_text').removeClass('hidden');
+      jQuery('.recommendation_intro.closed').siblings('.recommendation_text').addClass('hidden');
 
       
     //}
@@ -35,16 +35,35 @@ jQuery(document).ready(function() {
 		//if (width < 625) {
 			var element = jQuery(this);
       if (element.hasClass('open')) {
-        element.siblings('.recommendation_extras').addClass('hidden');
+        element.siblings('.recommendation_text, .recommendation_extras').addClass('hidden');
         element.removeClass('open');
         element.addClass('closed');
       }
       else {
-        element.siblings('.recommendation_extras').removeClass('hidden');
+        element.siblings('.recommendation_text, .recommendation_extras').removeClass('hidden');
         element.removeClass('closed');
         element.addClass('open');
         var offset = $(this).offset().top;
         jQuery('html, body').animate({ scrollTop: offset }, 400); // Scroll to the correct section
+      }
+		//}
+  });
+  
+  jQuery('.show_kunnskapsgrunnlag').on('click', function() {
+    var width = jQuery(window).width();
+		//if (width < 625) {
+			var element = jQuery(this);
+      if (element.hasClass('open')) {
+        element.siblings('.recommendation_kunnskapsgrunnlag').addClass('hidden');
+        element.removeClass('open');
+        element.addClass('closed');
+        jQuery(this).text('Vis kunnskapsgrunnlaget');
+      }
+      else {
+        element.siblings('.recommendation_kunnskapsgrunnlag').removeClass('hidden');
+        element.removeClass('closed');
+        element.addClass('open');
+        jQuery(this).text('Skjul kunnskapsgrunnlaget');
       }
 		//}
   });
@@ -66,10 +85,49 @@ jQuery(document).ready(function() {
     closed: 'accordion', // Close the panels on start, the options 'accordion' and 'tabs' keep them closed in there respective view types
    });
    
-   /**
    
-   jQuery('#local-nav').affix({
-   	offset: { top: $('#local-nav').offset().top }
-	});**/
+   
+   // Twitter typeahead
+   var substringMatcher = function(strs) {
+		 return function findMatches(q, cb) {
+			 var matches, substringRegex;
+ 
+			 // an array that will be populated with substring matches
+			 matches = [];
+ 
+			 // regex used to determine if a string contains the substring `q`
+			 substrRegex = new RegExp(q, 'i');
+ 
+			 // iterate through the pool of strings and for any string that
+			 // contains the substring `q`, add it to the `matches` array
+			 $.each(strs, function(i, str) {
+				 if (substrRegex.test(str)) {
+					 // the typeahead jQuery plugin expects suggestions to a
+					 // JavaScript object, refer to typeahead docs for more info
+					 matches.push({ value: str });
+				 }
+			 });
+ 
+			 cb(matches);
+			 };
+		};
+		
+	var keywords = ['sepsis', 'spedbarnsernæring', 'antibiotika', 'amming', 'amme', 'blodforgiftning', 'diabetes'];		
+		
+	$('#search_input .typeahead').typeahead({
+  hint: false,
+  highlight: true,
+  minLength: 1
+	},
+	{
+  	name: 'keywords',
+		displayKey: 'value',
+		source: substringMatcher(keywords)
+	});
+	
+	$('#search_input').bind('typeahead:selected', function(obj, datum, name) {      
+        window.location.href = 'soeketreff.html';
+	});
+	
    
 });
