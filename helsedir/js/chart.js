2b33c5d0e48ae7becbb096d9905a886953f29d54
ehallvard <haer@steria.no>
2014-05-29 15:12:59 +0200

    Endring i statistikk

--- helsedir/js/chart.js
+++ helsedir/js/chart.js
@@ -2,25 +2,31 @@
 google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-      ['Måned', 'Somatikk', 'VOP', 'BUP', 'RUS'],
-      ['mar 13', 44, 36, 45, 35],
-      ['apr 13', 52, 41, 52, 42],
-      ['mai 13', 50, 39, 55, 42],
-      ['jun 13', 48, 40, 50, 42],
-      ['jul 13', 39, 33, 47, 42],
-      ['aug 13', 61, 55, 68, 36],
-      ['sep 13', 65, 39, 64, 38],
-      ['okt 13', 49, 35, 35, 35],
-      ['nov 13', 49, 35, 36, 36],
-      ['des 13', 44, 35, 39, 37],
-      ['jan 14', 56, 48, 50, 48],
-      ['feb 14', 52, 34, 48, 34],
-      ['mar 14', 47, 38, 42, 38]
+      ['Måned', 'Somatikk', 'VOP', 'BUP', 'RUS', 'IPA'],
+      ['mar 13', 44, 36, 45, 35, 22],
+      ['apr 13', 52, 41, 52, 42, 24],
+      ['mai 13', 50, 39, 55, 42, 26],
+      ['jun 13', 48, 40, 50, 42, 28],
+      ['jul 13', 39, 33, 47, 42, 30],
+      ['aug 13', 61, 55, 68, 36, 32],
+      ['sep 13', 65, 39, 64, 38, 34],
+      ['okt 13', 49, 35, 35, 35, 32],
+      ['nov 13', 49, 35, 36, 36, 30],
+      ['des 13', 44, 35, 39, 37, 28],
+      ['jan 14', 56, 48, 50, 48, 26],
+      ['feb 14', 52, 34, 48, 34, 24],
+      ['mar 14', 47, 38, 42, 38, 22]
     ]);
 
     var options = {
         vAxis: { minValue: 20, maxValue: 70, title: "Antall dager", ticks: [20, 30, 40, 50, 60, 70] },
-        'chartArea': { 'width': '75%', 'height': '80%', 'left': '10%' }
+        'chartArea': { 'width': '75%', 'height': '80%', 'left': '10%' },
+        colors: ['#004e66', '#0093a7', '#6cb4b5', "#df9922", '#a1bf37'],
+        series: {
+            0: { lineDashStyle: [14, 7] },
+            1: { lineDashStyle: [5, 5] },
+            4: { lineDashStyle: [7, 14] }
+        }, lineWidth: 3
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
