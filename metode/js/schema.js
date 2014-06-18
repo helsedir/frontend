05694000dd470e6df87fd0b95af6517332f873f4
ehallvard <haer@steria.no>
2014-06-18 11:17:59 +0200

    Laget skjema for metode

---
+++ metode/js/schema.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $('#metodeibrukutfyllendetekst').addClass('visuallyhidden');
+
+    $('input:radio[name="metodeibruk"]').change(
+    function () {
+        if ($(this).val() == 'yes') {
+            $('#metodeibrukutfyllendetekst').slideUp(0, function () {
+                $('#metodeibrukutfyllendetekst').removeClass('visuallyhidden').slideDown(500);
+            });
+        }
+        else {
+            $('#metodeibrukutfyllendetekst').slideUp('fast', function () {
+                $('#metodeibrukutfyllendetekst').addClass('visuallyhidden').slideDown(0);
+            });
+        }
+    });
+});
