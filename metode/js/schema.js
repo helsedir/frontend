a5b929f5bc4a0dc1392a3065c22a91b853a9eb6c
ehallvard <haer@steria.no>
2014-06-18 15:24:04 +0200

    Stor oppdatering på skjema

--- metode/js/schema.js
+++ metode/js/schema.js
@@ -1,5 +1,8 @@
 ﻿$(function () {
     $('#metodeibrukutfyllendetekst').addClass('visuallyhidden');
+    $('#annet_utfyllendetekst').addClass('visuallyhidden');
+    $('#straling_utfyllendetekst').addClass('visuallyhidden');
+    $('#produsentleverandoertekst').addClass('visuallyhidden');
 
     $('input:radio[name="metodeibruk"]').change(
     function () {
@@ -11,6 +14,42 @@
         else {
             $('#metodeibrukutfyllendetekst').slideUp('fast', function () {
                 $('#metodeibrukutfyllendetekst').addClass('visuallyhidden').slideDown(0);
+            });
+        }
+    });
+
+    $('#annet').change(function () {
+        if (this.checked) {
+            $('#annet_utfyllendetekst').slideUp(0, function () {
+                $('#annet_utfyllendetekst').removeClass('visuallyhidden').slideDown(500);
+            });
+        } else {
+            $('#annet_utfyllendetekst').slideUp('fast', function () {
+                $('#annet_utfyllendetekst').addClass('visuallyhidden').slideDown(0);
+            });
+        }
+    });
+
+    $('input:radio[name="straaling"]').change(function () {
+        if ($(this).val() == 'yes') {
+            $('#straling_utfyllendetekst').slideUp(0, function () {
+                $('#straling_utfyllendetekst').removeClass('visuallyhidden').slideDown(500);
+            });
+        } else {
+            $('#straling_utfyllendetekst').slideUp('fast', function () {
+                $('#straling_utfyllendetekst').addClass('visuallyhidden').slideDown(0);
+            });
+        }
+    });
+
+    $('input:radio[name="produsentleverandoer"]').change(function () {
+        if ($(this).val() == 'yes') {
+            $('#produsentleverandoertekst').slideUp(0, function () {
+                $('#produsentleverandoertekst').removeClass('visuallyhidden').slideDown(500);
+            });
+        } else {
+            $('#produsentleverandoertekst').slideUp('fast', function () {
+                $('#produsentleverandoertekst').addClass('visuallyhidden').slideDown(0);
             });
         }
     });
