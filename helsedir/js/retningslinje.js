7d97081711ad400fe917c63b49f5142338ca1aa6
Sjur Seibt <sjurlur@gmail.com>
2014-09-11 12:59:26 +0200

    live

--- helsedir/js/retningslinje.js
+++ helsedir/js/retningslinje.js
@@ -1,4 +1,16 @@
-
+var retningslinjer = {
+	getData: function(url){
+		var viewModel;
+		$.getJSON(url, function(data){
+			viewModel = ko.mapping.fromJS(data);
+			ko.mapping.fromJS(data, viewModel);
+			ko.applyBindings(viewModel, document.getElementById("htmlTop"));
+			
+			//$('#toc').html(createTOC());
+			//console.log(createTOC());
+		});
+	}
+};
 
 jQuery(document).ready(function() {
 
