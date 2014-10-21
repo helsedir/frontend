b0e9e79e453bbee203301358b1151f668adae090
Sjur Seibt <sjurlur@gmail.com>
2014-10-21 16:44:12 +0200

    Revert "alle - fikset css-bugs i henhold til tilbakemeldinger"
    This reverts commit bf587423403b1d463cad52e38063a573c6b1f3d8.

--- ehelse/js/helsedir.js
+++ ehelse/js/helsedir.js
@@ -87,7 +87,7 @@
         }
     });
     //styles tables. workaround for IE8
-    $('table.ms-rteTable-default tr:nth-child(2n+1)').addClass('odd');
+    $('table.table_general tr:nth-child(2n+1)').addClass('odd');
 
     //show/hide more news/conferences
     $('.showmorenews').on('click', function () {
--- metode/js/helsedir.js
+++ metode/js/helsedir.js
@@ -141,7 +141,7 @@
         }
     });
     //styles tables. workaround for IE8
-    $('table.ms-rteTable-default tr:nth-child(2n+1)').addClass('odd');
+    $('table.table_general tr:nth-child(2n+1)').addClass('odd');
 
     $('#menushortcut').on('click', function () {
         var expandedContent = $(".js-expand");
